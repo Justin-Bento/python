01a7345bda66709e7ec326de277f32654a98cd40
Justin Bento <bento.a.justin@gmail.com>
2022-06-22 13:25:47 -0400

    broke down the game into variious rules

---
+++ rock-paper-scissors.py
@@ -0,0 +1,11 @@
+"""
+  - [] Think of an issue that needs to be settled.
+  - [] Face off against bot.
+  - [] Count down then enter make your play
+  - [] Play one of the three objects against your opponent
+    - [] Rock wins against scissorss
+    - [] Scissors win against paper.
+    - [] Paper wins against rock.
+    - [] If rock paper and sicorss tie make it a draw.
+  - [] Determine the winner after through best of 3.
+ """
