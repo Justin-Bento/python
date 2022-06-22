0b2eeb9c2c7773f253a82237caa25bf27eafe69b
Justin Bento <bento.a.justin@gmail.com>
2022-06-22 11:46:10 -0400

    fixed spelling for final greeting message

--- greetings.py
+++ greetings.py
@@ -2,7 +2,7 @@ def main():
     print("Hello World")
     name = input("What is your name? ")
     print("Nice to meet you", name)
-    print("Hope you have a great time learning ppython", name)
+    print("Hope you have a great time learning python", name)
 
 if __name__ == "__main__":
     main()
