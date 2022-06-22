ff4213da79f5f617e403dd461b02cf7013dbabe0
Justin Bento <bento.a.justin@gmail.com>
2022-06-22 11:51:10 -0400

    Imported Time Delays to exit code.
      - Added delay to final message.
      - Added exit function at the end

--- greetings.py
+++ greetings.py
@@ -1,8 +1,13 @@
+import time
+
 def main():
     print("Hello World")
     name = input("What is your name? ")
     print("Nice to meet you", name)
-    print("Hope you have a great time learning python", name)
+    time.sleep(1)
+    print("Bot: Hope you have a great time learning python", name)
 
 if __name__ == "__main__":
     main()
+
+exit()
