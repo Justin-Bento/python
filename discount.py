80f8939d965b3c2e5a02390eebfe2db78308ef46
Justin Bento <bento.a.justin@gmail.com>
2022-06-23 10:19:47 -0400

    Added multiline comment.
    Doing this makes the challange comments easier to read.

--- discount.py
+++ discount.py
@@ -1,1 +1,7 @@
-# Create a function in Python that accepts two parameters. The first should be the full price of an item as an integer. The second should be the discount percentage as an integer. The function should return the price of the item after the discount has been applied. For example, if the price is 100 and the discount is 20, the function should return 80.
+"""
+  Create a function in Python that accepts two parameters.
+  The first should be the full price of an item as an integer.
+  The second should be the discount percentage as an integer.
+  The function should return the price of the item after the discount has been applied.
+  For example, if the price is 100 and the discount is 20, the function should return 80.
+"""
