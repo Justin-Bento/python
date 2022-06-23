7a877fddd3ce3ba80cddaa5998edf44b683c7256
Justin Bento <bento.a.justin@gmail.com>
2022-06-23 10:22:10 -0400

    created a function to return the full price of a product

--- discount.py
+++ discount.py
@@ -5,3 +5,7 @@
   The function should return the price of the item after the discount has been applied.
   For example, if the price is 100 and the discount is 20, the function should return 80.
 """
+
+def full_price(number):
+  print("The most common number between 1 and 10 is " + str(number))
+full_price(7)
