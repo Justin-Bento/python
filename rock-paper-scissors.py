daaf77ea2e38ff6ecb0ff1090b36d1d90e73f775
Justin Bento <bento.a.justin@gmail.com>
2022-07-04 12:23:36 -0400

    created a rock paper scissors script with included steps

---
+++ rock-paper-scissors.py
@@ -0,0 +1,6 @@
+"""
+- [] Random function: to generate rock, paper, or scissors. 
+- [] Valid function: to check the validity of the move.
+- [] Result function: to declare the winner of the round.
+- [] Scorekeeper: to keep track of the score.
+"""
