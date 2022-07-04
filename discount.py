d2ca77c1f2070c82aebc6dcd0aeb7b818f3fc848
Justin Bento <bento.a.justin@gmail.com>
2022-07-04 11:49:29 -0400

    calculated sale price with grand total from recipte.
    - created a variable called result and oupted the discount and total.
    - outputted the result by dividing the discount and total.
    - changed user input into a float rather than a striing.
    - Updated the print statements to use template literials.

--- discount.py
+++ discount.py
@@ -7,16 +7,18 @@
 """
 
 def grand_total():
-  global total
-  total = input("What is the total for groceries amount after tax? \n")
-  print("The grand total is, " + total + "$")
+    global total
+    total = input("What is the total for groceries amount after tax? \n")
+    print("The grand total is, " + total + "$")
 grand_total()
 
 def sale_price():
-  sale = input("Was there any sale today? \n")
-  if sale in [True, "Yes", "yes"]:
-    discount = input("What is the discount for groceries after tax? \n")
-    print("The discount is, " +  discount + "%")
-  else:
-    print("Your total is stil " + total + "$")
+    sale = input("Was there any sale today? \n")
+    if sale in [True, "Yes", "yes"]:
+        discount = input("What is the discount for groceries after tax? \n")
+        print("The discount is, " + discount + "%")
+        result = float(total) / float(discount)
+        print(f'Your new total is: {result}$')
+    else:
+        print(f'Your total is stil {total}$')
 sale_price()
