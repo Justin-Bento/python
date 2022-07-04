40c05f712c954192d270ab9e007ebb85249e91ea
Justin Bento <bento.a.justin@gmail.com>
2022-07-04 12:28:12 -0400

    created a bunch of functions for rock paper sicssors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -4,3 +4,19 @@
 - [] Result function: to declare the winner of the round.
 - [] Scorekeeper: to keep track of the score.
 """
+
+def random():
+  print("Hello from a random function")
+random()
+
+def validate():
+  print("Hello from a validate function")
+validate()
+
+def result():
+  print("Hello from a result function")
+result()
+
+def scorekeeper():
+  print("Hello from a function")
+scorekeeper()
