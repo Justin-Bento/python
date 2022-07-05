0115f36eb1fc46120dabe9dc8dfd463b71d9f935
Justin Bento <bento.a.justin@gmail.com>
2022-07-05 11:56:00 -0400

    fixed indentation in python

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -23,29 +23,28 @@ def game_logic():
   scorekeeper = 1
 
   while player == False:
-  #set player to True
-      # If a player option types in Rock, Paper or Scissors this logic will determin who wins or looses if a user types in anything else the loop will close.
-      player = input("Rock, Paper, Scissors?")
-      if player == computer:
-          print("Tie!")
-      elif player == "Rock":
-          if computer == "Paper":
-              print("You lose!", computer, "covers", player)
-          else:
-              print("You win!", player, "smashes", computer)
-      elif player == "Paper":
-          if computer == "Scissors":
-              print("You lose!", computer, "cut", player)
-          else:
-              print("You win!", player, "covers", computer)
-      elif player == "Scissors":
-          if computer == "Rock":
-              print("You lose...", computer, "smashes", player)
-          else:
-              print("You win!", player, "cut", computer)
-      else:
-          print("That's not a valid play. Check your spelling!")
-          break
-      player = False
-      computer = options[randint(0,2)]
+    # If a player option types in Rock, Paper or Scissors this logic will determin who wins or looses if a user types in anything else the loop will close.
+    player = input("Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+        break
+    player = False
+    computer = options[randint(0,2)]
 game_logic()
