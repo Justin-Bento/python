454b14b823062b16b2c699de496db56aea3506c7
Justin Bento <bento.a.justin@gmail.com>
2022-07-06 22:00:22 -0400

    included steps on customer service and interaction

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,6 +1,11 @@
 """
-Create a tip calculator:
-  - [] Get total amount of of the bill.
-  - [] Include the tip amount in percentage.
-  - [] Display new amount with tip included.
-"""
+ - [] Create a generic greeting to welcome the customer.
+ - [] Make an array of items for the shop.
+ - [] Wait for customer to make a decision.
+ - [] Mention the price and ask how would the customer like to pay. only accept intergers or floats.
+ - [] Wait to see how the customer would like to pay and then include the total after tax.
+ - [] Create a function to calculate the customers cash amount after paying.
+ - [] Ask if they would like to include a tip. -- If they do give a percentage and include a new price, if not move onto next step.
+ - [] Make a generic greeting to the customer for purchasing at store.
+ - [] Exit program.
+ """
