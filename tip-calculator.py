750976f7d089d3e4dbcb434bf663729859ef440e
Justin Bento <bento.a.justin@gmail.com>
2022-07-07 09:36:46 -0400

    Added fake menu using JSON data to store mutiple items.
     The JSON Array include object that store a product name, price and the
     ID for order, Doing this keeps everything organised

--- tip-calculator.py
+++ tip-calculator.py
@@ -13,6 +13,84 @@
 # imported timer module to sleep on final greeting
 import time
 
+bakery = [
+  {
+    id: 1,
+    product: 'Black Forest Ham',
+    price: 9.70,
+  },
+  {
+    id: 2,
+    product: 'bacon, lettuce, and tomato',
+    price: 9.95,
+  },
+  {
+    id: 3,
+    product: 'Roast Beef or Turkey',
+    price: 9.70,
+  },
+  {
+    id: 4,
+    product: 'Tuna Salad',
+    price: 9.70,
+  },
+  {
+    id: 5,
+    product: 'Egg Salad',
+    price: 8.90,
+  },
+  {
+    id: 6,
+    product: 'Swis Or Chedder Cheese',
+    price: 8.60,
+  },
+  {
+    id: 7,
+    product: 'Brie',
+    price: 9.75,
+  },
+  {
+    id: 8,
+    product: 'Grilled Vegtables',
+    price: 9.25
+  },
+  {
+    id: 9,
+    product: 'Garden Vegtables',
+    price: 7.25
+  },
+  {
+    id: 10,
+    product: 'Bagel With Cheese Sandwitch',
+    price: 7.50,
+  },
+  {
+    id: 11,
+    product: 'Bagel & Butter',
+    price: 2.55,
+  },
+  {
+    id: 12,
+    product: 'Bagel and Cream Cheese',
+    price: 4.00,
+  },
+  {
+    id: 13,
+    product: 'Bagel With Ham',
+    price: 9.70,
+  },
+  {
+    id: 14,
+    product: 'Omelette',
+    price: 6.95,
+  },
+  {
+    id: 15,
+    product: 'Bagel Eggs and Toast',
+    price: 5.70,
+  }
+]
+
 def introduction():
  print("Hello Welcome To Bot-World!")
  name = input("Can I ask you for your name?")
