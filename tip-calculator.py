a3314e1e17d7b39a0ba5c484f6d34daf2fe6aa78
Justin Bento <bento.a.justin@gmail.com>
2022-07-08 10:29:25 -0400

    moved json data into its own file
    - Removed json variable.
    - Created resturant menu in json file.
    - Imported json module and file to pproject to open and read the project

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,7 +1,7 @@
 """
  - [x] Create a generic greeting to welcome the customer.
  - [x] Make an array of items for the shop.
- - [] Wait for customer to make a decision.
+ - [] Wait for customer to make a decision. If the decision is making Bot loose customers quit program.
  - [] Mention the price and ask how would the customer like to pay. only accept intergers or floats.
  - [] Wait to see how the customer would like to pay and then include the total after tax.
  - [] Create a function to calculate the customers cash amount after paying.
@@ -12,84 +12,10 @@
 
 # imported timer module to sleep on final greeting
 import time
+import json
 
-bakery = [
-  {
-    'id': 1,
-    'product': 'Black Forest Ham',
-    'price': 9.70,
-  },
-  {
-    'id': 2,
-    'product': 'bacon, lettuce, and tomato',
-    'price': 9.95,
-  },
-  {
-    'id': 3,
-    'product': 'Roast Beef or Turkey',
-    'price': 9.70,
-  },
-  {
-    'id': 4,
-    'product': 'Tuna Salad',
-    'price': 9.70,
-  },
-  {
-    'id': 5,
-    'product': 'Egg Salad',
-    'price': 8.90,
-  },
-  {
-    'id': 6,
-    'product': 'Swis Or Chedder Cheese',
-    'price': 8.60,
-  },
-  {
-    'id': 7,
-    'product': 'Brie',
-    'price': 9.75,
-  },
-  {
-    'id': 8,
-    'product': 'Grilled Vegtables',
-    'price': 9.25
-  },
-  {
-    'id': 9,
-    'product': 'Garden Vegtables',
-    'price': 7.25
-  },
-  {
-    'id': 10,
-    'product': 'Bagel With Cheese Sandwitch',
-    'price': 7.50,
-  },
-  {
-    'id': 11,
-    'product': 'Bagel & Butter',
-    'price': 2.55,
-  },
-  {
-    'id': 12,
-    'product': 'Bagel and Cream Cheese',
-    'price': 4.00,
-  },
-  {
-    'id': 13,
-    'product': 'Bagel With Ham',
-    'price': 9.70,
-  },
-  {
-    'id': 14,
-    'product': 'Omelette',
-    'price': 6.95,
-  },
-  {
-    'id': 15,
-    'product': 'Bagel Eggs and Toast',
-    'price': 5.70,
-  }
-]
+# Opening JSON file
+# f = open('data.json')
 
 def introduction():
   print("Hello Welcome To Bot-World!")
