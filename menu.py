3e506652fd259fb28df35946977a7df1a5eb846a
Justin Bento <bento.a.justin@gmail.com>
2022-07-10 14:53:17 -0400

    created a menu.py to make a terminal menu
    For practice for the tip-calculator

---
+++ menu.py
@@ -0,0 +1,10 @@
+"""
+ -[] Create an Array of fruits.
+     - Dislay the options in a list format. 
+     - Allow the user to select the menu through typing the response. 
+ -[] Create User select menu. 
+    - Selecting a correct awnser will store and output the selected items.
+    - Selecting an invalid option will display the menu; 3 invalid answers will quit the program. 
+-[] Create a closing script for the user to access at any time.  
+    - Use the Q shortcut to quit.
+"""
