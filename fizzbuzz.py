4d287dfcd6b8ac929ede2c70211ab5d8a7a71c88
Justin Bento <bento.a.justin@gmail.com>
2022-07-13 19:13:27 -0400

    updated wording for comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,20 +5,20 @@
  - [x] For each multiple of 5, print "Buzz" instead of the number.
 """
 
-# Created a for lopp that starts at 1 and ends at 100.
-for number in range(1, 100):
-    # prined the results of the range
-    print(number)
-    # if the number 3 and 5 is devided and returned by denominator that can multiply between both of them it will print Fizz Buzz.
+# Return a sequence of numbers from 0 to 100.
+count = range(0, 100)
+
+# Lopp through the count's ranage.
+for number in count:
+    # Print FizzBuzz is both 3 and 5 are multpiles of each other.
     if number % 3 == 0 and number % 5 == 0:
         print('FizzBuzz')
-    # if the number is devided and returned by 3 it will print out Fizz instead of a number.
+    # Print Fizz if the number is divided by 3.
     elif number % 3 == 0:
         print('Fizz')
-    # if the number is devided and returned by 5 it will print out Fizz instead of a number.
+    # Print Bizz if the number is divided by 5.
     elif number % 5 == 0:
         print('Buzz')
 
-
-# Command to exit the loop and not waist extra resources.
-exit()
+# print the results
+print(number)
