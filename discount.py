7163c5395d58386322314756a0107ae96bec0013
Justin Bento <bento.a.justin@gmail.com>
2022-07-13 19:20:25 -0400

    updated wording for comments

--- discount.py
+++ discount.py
@@ -7,28 +7,26 @@
 """
 
 def grand_total():
-    # Created a global variable called total.
-    global total
-      # Added the total as an input to grab and collect the a final price of a receipt. 
-    total = input("What is the total for groceries amount after tax? \n")
-      # Then printed out the current grand total, to confirm that this is the amount a user inputed.
-    print(f'The grand total is: {total}$')
+  # Created a global variable.
+  global total
+  # Collects the a final price of a receipt. 
+  total = input("What is the total for groceries amount after tax? \n")
+  # Prints out the amount a user inputed.
+  print(f'The grand total is: {total}$')
 grand_total()
 
-#  Created a seprate function to determin discounts.
+# This function determins discount price.
 def sale_price():
-    # If the user says there was a sale it would ask you for the percentage. Eg. a seniors discount in Canada 10%,
-    # Then it would output the discount amount notifing the user that it has been stored.
-    sale = input("Was there any sale today? \n")
-    if sale in [True, "Yes", "yes"]:
-        discount = input("What is the discount for groceries after tax? \n")
-        print(f'The discount is: {discount}%')
-        # A new total would be calculate and printed by dividing the grand_total and sale_percentage the user inputed.
-        result = float(total) / float(discount)
-        print(f'Your new total is: {result}$')
-      # If there was no sale your total would still be the same.
-    else:
-        print(f'Your total is stil: {total}$')
+  # User Input Verifies if a sale took place,
+  sale = input("Was there any sale today? \n")
+  # If there was a sale it would output the discount amount. 
+  if sale in [True, "Yes", "yes"]:
+      discount = input("What is the discount for groceries after tax? \n")
+      print(f'The discount is: {discount}%')
+      # Sale is calculated by dividing the grand_total and sale_percentage the user inputed.
+      result = float(total) / float(discount)
+      print(f'Your new total is: {result}$')
+    # If there was no sale it would output the origional amount. 
+  else:
+      print(f'Your total is stil: {total}$')
 sale_price()
-
-# This closes the program to ensure nothing is runing.
