8def011965d9c2254f49a20c1e31113674eb9413
Justin Bento <bento.a.justin@gmail.com>
2022-07-13 19:25:26 -0400

    updated wording for comments

--- greetings.py
+++ greetings.py
@@ -1,25 +1,20 @@
 # imported timer module to sleep on final greeting
 import time
 
-# Created a function call main
+# This function handles the greeting. 
 def main():
-
-    """
-      # Printed greetinig for computer.
-      # Created a variable for input from the user.
-      # Added sleep timer to delay final message.
-      # Closed off final conversaton.
-    """
-
-    print("Hello World")
-    name = input("What is your name? ")
-    print("Nice to meet you", name)
-    time.sleep(1)
+    # Prints a Hello World greeting 
+    print("Bot: Hello World")
+    # create an name variable to collect your name.  
+    name = input("Bot: What is your name? ")
+    # Greets you with the words you typed.  
+    print("Bot: Nice to meet you", name)
+    # Delays the final message for 0.40s  
+    time.sleep(0.40)
+    # Displays final message  
     print("Bot: Hope you have a great time learning python", name)
 
 # make sure the function is only beinig used when called.
 if __name__ == "__main__":
     main()
-
-# Close other background processes
-exit()
+    
