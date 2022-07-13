49bf569a270499869ae357167139cfcbf1634287
Justin Bento <bento.a.justin@gmail.com>
2022-07-13 14:06:44 -0400

    Created a fetch api file
    Guide to learn how to fetch json data
    Included comments on what needs to be done

---
+++ fetch-api.py
@@ -0,0 +1,6 @@
+"""
+ Pick a random application pprogramming interface.
+ Fetch the data from that A.P.I and log it into the console. 
+ Display a error message if data is not found if the data is found display a success message.
+ Allow the user to input specific infromation reguaring the data set.
+"""
