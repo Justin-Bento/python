6cb4d6670f2995c6628cd36ce441c3c2c7c098b0
Justin Bento <bento.a.justin@gmail.com>
2022-07-13 15:13:32 -0400

    fetched data to grab user data
    1.  send a request to grab url
    2. check connection status to see if infromation is successful
    3. created a class that held info
    4. print a basic greeting

--- fetch-api.py
+++ fetch-api.py
@@ -5,12 +5,30 @@
  Allow the user to input specific infromation reguaring the data set.
 """
 import requests
+import time
 
-res = requests.get('https://randomuser.me/api')
+url = f'https://randomuser.me/api'
+res = requests.get(url)
+
+print(' ')
 
 if res.status_code == 200:
+  time.sleep(0.600)
   print('Successful connection')
+  time.sleep(0.600)
+  print(' ')
   print(res.json())
 else:
   print('No connection')
 
+print(' ')
+
+class user:
+  first_name = res.json()['results'][0]['name']['first']
+  last_name = res.json()['results'][0]['name']['last']
+  age = res.json()['results'][0]['dob']['age']
+  gender = res.json()['results'][0]['gender']
+
+print(f'Hi. My name is {user.first_name} {user.last_name} and I am {user.age} years old {user.gender}.')
+
+print(' ')
