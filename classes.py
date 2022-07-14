3f84318edba3f07be539ff005d787b9870a371c5
Justin Bento <bento.a.justin@gmail.com>
2022-07-14 14:10:30 -0400

    replaced smartphone with person  attrinutes

--- classes.py
+++ classes.py
@@ -1,18 +1,17 @@
 """
-    Create a class named smartphone.
-    Declair  brand name and module number of the Smartphone class.
-    Display properties of the inputed smartphone brand and modle.
-    Allow the user to input various enter infromation for a new smartphone.
-    Allow the user to rmeove input various enter infromation for a new smartphone.
+    Create a class named person.
+    Declair  brand name and module number of the person class.
+    Display properties of the inputed person brand and modle.
+    Allow the user to input various enter infromation for a new person.
+    Allow the user to rmeove input various enter infromation for a new person.
 """
 
-# Smartphone class will collect and store properties inside a phone.
-class Smartphone:
-    def __init__(self, display, battery, processor, storage, RAM, camera, sensors):
-        self.display = display
-        self.battrey = battrey
-        self.processor = processor
-        self.storage = storage
-        self.RAM = RAM
-        self.camera = camera
-        self.sensors = sensors
+# person class will collect and store properties inside a phone.
+class person:
+    def __init__(self, name, age, sex, nat):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.nat = nat
+    def collect_data(self):
+        input('Enter personal infromation: ')
