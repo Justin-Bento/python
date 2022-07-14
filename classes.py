371e31f84d346e5978d1ccea149572651503aea7
Justin Bento <bento.a.justin@gmail.com>
2022-07-14 13:57:59 -0400

    created a class file to get used to python classes

---
+++ classes.py
@@ -0,0 +1,7 @@
+"""
+    Create a class named smartphone.
+    Declair  brand name and module number of the Smartphone class.
+    Display properties of the inputed smartphone brand and modle.
+    Allow the user to input various enter infromation for a new smartphone.
+    Allow the user to rmeove input various enter infromation for a new smartphone.
+"""
