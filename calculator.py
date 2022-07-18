8b316400cfa7fdf9747240ad29bab62bcacc8d18
Justin Bento <bento.a.justin@gmail.com>
2022-07-18 19:41:27 -0400

    added functions for basic math operators

--- calculator.py
+++ calculator.py
@@ -1,12 +1,24 @@
 """
-  - [] Program make a simple calculator.
-  - [] Creat a function to handle these math functions.
-       - [] Add.
-       - [] Subtract.
-       - [] Multiplication.
-       - [] Divide.
+  - [x] Program make a simple calculator.
+  - [x] Creat a function to handle these math functions.
+       - [x] Add.
+       - [x] Subtract.
+       - [x] Multiplication.
+       - [x] Divide.
   - [] Take input from the user for an list of numbers.
   - [] Ask the user to Select the operation.
   - [] Print out calulation.
   - [] Check if user wants another calculation and close if answer is no.
 """
+
+def addition(first_num, second_num):
+  return first_num + second_num
+
+def subtraction(first_num, second_num):
+  return first_num - second_num
+
+def division(first_num, second_num):
+  return first_num / second_num
+
+def multiplication(first_num, second_num):
+  return first_num * second_num
