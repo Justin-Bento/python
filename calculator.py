90a1a6c250870796110d19b40886f54e0c3acf43
Justin Bento <bento.a.justin@gmail.com>
2022-07-18 19:36:28 -0400

    created a calulator file with to do tasks

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+  - [] Program make a simple calculator.
+  - [] Creat a function to handle these math functions.
+       - [] Add.
+       - [] Subtract.
+       - [] Multiplication.
+       - [] Divide.
+  - [] Take input from the user for an list of numbers.
+  - [] Ask the user to Select the operation.
+  - [] Print out calulation.
+  - [] Check if user wants another calculation and close if answer is no.
+"""
