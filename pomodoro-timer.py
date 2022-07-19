064a19913da7ce679747716cb277240ffa8d7036
Justin Bento <bento.a.justin@gmail.com>
2022-07-18 22:54:41 -0400

    added to-do list for pomodroro timer

---
+++ pomodoro-timer.py
@@ -0,0 +1,20 @@
+"""
+  - [] Create a timer that breaks down in 4 intervals.
+       - [] Main work interval. - 25 minutes
+       - [] Main short break interval. 5 minutes.
+       - [] Main long break interval. 30 minutes.
+       - [] Repeat Interaction. 4 times
+       - [] After 4 times close or repeate program.
+
+  - [] Ask the user if they want defualt or custom setting.
+       - [] Change break and interval time if yes 
+       - [] Keep defult break and interval time if no 
+  
+  - [] System notification for completed times.
+       - [] Have a title for short break, work and long break 
+       - [] Have a message for short break, work and long break 
+
+  - [] Ask the user if they would like to repeat interaction.
+       - [] if user says y repeat interaction.
+       - [] if user says no break and close the program.
+"""
