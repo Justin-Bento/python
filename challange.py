4484367cc3b2ee4cf036734fef866a0b5b088b1d
Justin Bento <bento.a.justin@gmail.com>
2022-07-20 10:04:18 -0400

    added a for loop that prins out days in a challange

---
+++ challange.py
@@ -0,0 +1,11 @@
+"""
+  - [] Create a variablle that holds a range variable.
+  - [] For loop that prints out the desired range.
+  - [] add a template literal for string.
+"""
+
+challange__90_days = range(1, 91)
+challange__30_days = range(1, 31)
+
+for number in challange__90_days:
+  print(f"Days {number}")
