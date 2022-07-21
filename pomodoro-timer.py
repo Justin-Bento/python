4c555d4eca63e137240dba3419c796855fe35c82
Justin Bento <bento.a.justin@gmail.com>
2022-07-21 13:23:22 -0400

    added a countdown timer to count in seconds

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -19,8 +19,16 @@
        - [] if user says no break and close the program.
 """
 
-def countdown(timer):
-     pass
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("stop")
+
+countdown(70)
 
 def system_notification(title, message, action):
      pass
