c8cbf68fa47c94483a3218359532e50f94e1524c
Justin Bento <bento.a.justin@gmail.com>
2022-07-21 10:10:31 -0400

    created a minute to second calulator

---
+++ min-to-sec.py
@@ -0,0 +1,16 @@
+"""
+  - [] Create a countdown function that only count downs in seconds.
+  - [] 
+"""
+
+import time
+
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("stop")
+
